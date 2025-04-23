d45501c920ea051eccee62652dc086ac95f9dbea
Juneja16 <rjhk6767@gmail.com>
2025-04-23 18:52:12 +0530

    Updated with MongoDB Atlas Setup

--- App/Config/database.js
+++ App/Config/database.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
 
-const DB_URI = process.env.DBURL;
+const DB_URI = process.env.DBURI;
 
 const connectDB = async () => {
   try {
